04b443119d0efe52b812476db04814450160b164
michaelsh6 <michaelsh6@gmail.com>
2019-11-12 22:21:59 +0200

    start

---
+++ mini_project_targil_1/part1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1
+{
+    class Program
+    {
+        static Random r = new Random();
+        const int ARR_LEN = 20, LOW = 18, HIGH = 122;
+
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0,-4}", arr[i]);
+            }
+            Console.WriteLine();
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            int[] A = new int[ARR_LEN], B = new int[ARR_LEN], C = new int[ARR_LEN];
+            for (int i = 0; i < ARR_LEN; i++)
+            {
+                A[i] = r.Next(LOW, HIGH);
+                B[i] = r.Next(LOW, HIGH);
+                C[i] = A[i] > B[i] ? A[i] - B[i] : B[i] - A[i];
+            }
+            Print(A);
+            Print(B);
+            Print(C);
+            Console.ReadKey();
+        }
+    }
+}
