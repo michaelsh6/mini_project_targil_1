5c5ce4b5b206d7a597a0b6ebbdc9c2c49613d0a2
michaelsh6 <michaelsh6@gmail.com>
2019-11-13 17:50:01 +0200

    start part 2

--- mini_project_targil_1/part2/Program.cs
+++ mini_project_targil_1/part2/Program.cs
@@ -8,9 +8,54 @@ namespace part2
 {
     class Program
     {
+
+        bool[,] calander = new bool[12, 31];
         static void Main(string[] args)
         {
-            Console.Write("test");
+            
+            Console.WriteLine("entr your choice:");
+            string user_input = Console.ReadLine();
+            switch(user_input)
+            {
+                case "REQUEST":
+                    AddVection();
+                    break;
+
+                case "LIST":
+                    break;
+
+                case "NUM_OF_DAYS":
+                    break;
+
+                case "EXIT":
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+
+            }
+
+        }
+
+        public static void AddVection()
+        {
+            Console.WriteLine("enter first date as format DD/MM");
+            String[] first_date = Console.ReadLine().Split('/');
+            Console.WriteLine("enter last date as format DD/MM");
+            String[] last_date = Console.ReadLine().Split('/');
+            int first_day = Int32.Parse(first_date[0]);
+            int first_month = Int32.Parse(first_date[1]);
+            int last_day = Int32.Parse(last_date[0]);
+            int last_month = Int32.Parse(last_date[1]);
+            for(int month = first_month-1; month < last_month; month++)
+            {
+                for(int day = first_month - 1; day < last_month; day++)
+                {
+
+                }
+            }
+                // throw new NotImplementedException();
         }
     }
 }
