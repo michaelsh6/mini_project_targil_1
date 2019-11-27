87158d26ac98e94838e519a3725a7273c1e05c20
shimizra <57220385+shimizra@users.noreply.github.com>
2019-11-27 16:51:28 +0200

    Add doc

--- mini_project_targil_2/mini_project_targil_2/GuestRequest.cs
+++ mini_project_targil_2/mini_project_targil_2/GuestRequest.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 
 namespace mini_project_targil_2
 {
+    //
+    //GuestRequest class:the class presents customer hosting requirement.
+    //
     class GuestRequest
     {
         
-        public const int YEAR  = 2019;
+        public static int YEAR = DateTime.Today.Year;
         //public static DateTime  FIRST_DATE = new DateTime(YEAR, 1, 1);
         //public static DateTime  LAST_DATE = new DateTime(YEAR, 12, 31);
 
@@ -18,24 +21,38 @@ namespace mini_project_targil_2
         public DateTime ReleaseDate;//{ get => ReleaseDate; set => ReleaseDate = value; }
 
         private bool m_IsApproved;
+        
+        /// <summary>
+        /// Property get\set of m_IsApproved field.
+        /// </summary>
         public bool IsApproved
         {
             get => m_IsApproved;
             set => m_IsApproved = value;
         }
+         /// <summary>
+        ///GuestRequest defult ctor.
+        /// </summary>
         public GuestRequest()
         {
             IsApproved = false;
         }
-
+        /// <summary>
+        /// num_of_day function.
+        /// </summary>
+        /// <returns>the function returns requested vacation term.</returns>
         public int num_of_day()
         {
             return (ReleaseDate - EntryDate).Days;
         }
 
+        /// <summary>
+        /// ToString function.
+        /// </summary>
+        /// <returns>the function returns string entry and exit vacation.</returns>
         public override string ToString()
         {
-            return $"Entry Date: {EntryDate.ToString("dd/MM")}, Release Date: {ReleaseDate.ToString("dd/MM")}";
+            return $"Entry Date: {EntryDate}, Release Date: {ReleaseDate}";
         }
 
        
