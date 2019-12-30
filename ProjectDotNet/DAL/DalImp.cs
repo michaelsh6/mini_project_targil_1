c04ed61665cdf1c6d8216f706e96bdc459e16505
shimizra <57220385+shimizra@users.noreply.github.com>
2019-12-30 12:11:09 +0200

    TODO מימוש מתודות, יש להמשיך עוד לפי

--- ProjectDotNet/DAL/DalImp.cs
+++ ProjectDotNet/DAL/DalImp.cs
@@ -1,78 +1,128 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BE;
+using DS;
 
 namespace DAL
 {
     public class DalImp :IDAL
     {
-        public void addGuest(Guest guest)
+
+        private bool CheckGuest(int id)
+        {
+            return dataSource.guests.Any(x => x.GuestRequestKey == id);
+        }
+
+        private bool CheckHostingUnit(int id)
+        {
+            return dataSource.hostingUnits.Any(x => x.HostingUnitKey == id);
+        }
+
+        private bool CheckOrder(int id)
+        {
+            return dataSource.orders.Any(x => x.OrderKey == id);
+        }
+
+
+        public void addGuest(Guest gst)
         {
             //IEnumerable<Guest> result = from item in DS.dataSource.guests
-            //             where item.GuestRequestKey == guest.GuestRequestKey
-            //             select item;
-            //    if(DS.dataSource.guests.Count(x => x.GuestRequestKey == guest.GuestRequestKey)==0)
-            //    {
-            //        throw new Exception("dssf"); //TODO // DuplicateIdException()
-            //    }
-
-            //    throw new NotImplementedException();
-            //
+            //                            where item.GuestRequestKey == guest.GuestRequestKey
+            //                            select item;
+            //if (DS.dataSource.guests.Any(x => x.GuestRequestKey == guest.GuestRequestKey))
+            //{
+            //    throw new Exception("a"); 
+            //}
+            if(!CheckGuest(gst.GuestRequestKey))
+            {
+                dataSource.guests.Add(gst.Clone());
+            }
+            else
+            {
+                throw new Exception("a"); //TODO // DuplicateIdException()
+            }
         }
 
-        public void updateGuest(Guest guest)
+        public void updateGuest(Guest gst)
         {
-            throw new NotImplementedException();
+            //int count = dataSource.guests.RemoveAll(x=>x.GuestRequestKey == gst.GuestRequestKey);
+            //if (count == 0)
+            //    throw new Exception("a"); 
+            dataSource.guests.RemoveAll(x => x.GuestRequestKey == gst.GuestRequestKey);
+            addGuest(gst);
         }
 
-        public void addHostingUnit(HostingUnit hostingUnit)
+        public void addHostingUnit(HostingUnit hstUnit)
         {
-            throw new NotImplementedException();
+            if (!CheckHostingUnit(hstUnit.HostingUnitKey))
+            {
+                dataSource.hostingUnits.Add(hstUnit.Clone());
+            }
+            else
+            {
+                throw new Exception("a"); //TODO // DuplicateIdException()
+            }
         }
 
-        public void updateHostingUnit(HostingUnit hostingUnit)
+        public void updateHostingUnit(HostingUnit hstUnit)
         {
-            throw new NotImplementedException();
+           dataSource.hostingUnits.RemoveAll(x => x.HostingUnitKey == hstUnit.HostingUnitKey);
+            addHostingUnit(hstUnit);
         }
 
-        public void deleteHostingUnit(HostingUnit hostingUnit)
+        public void deleteHostingUnit(int hstUnitId)
         {
-            throw new NotImplementedException();
+            int count = dataSource.hostingUnits.RemoveAll(x => x.HostingUnitKey == hstUnitId);
+            if (count == 0)
+                throw new Exception("a"); //TODO // DuplicateIdException()
         }
 
-        public void addOrder(Order order)
+        public void addOrder(Order ord)
         {
-            throw new NotImplementedException();
+            if (!CheckOrder(ord.OrderKey))
+            {
+                dataSource.orders.Add(ord.Clone());
+            }
+            else
+            {
+                throw new Exception("a"); //TODO // DuplicateIdException()
+            }
         }
 
-        public void updateOrder(Order order)
+        public void updateOrder(Order ord)
         {
-            throw new NotImplementedException();
+            //int count = dataSource.orders.RemoveAll(x => x.OrderKey == ord.OrderKey);
+            //if (count == 0)
+            //    throw new Exception("a");  
+            dataSource.orders.RemoveAll(x => x.OrderKey == ord.OrderKey);
+            addOrder(ord);
         }
 
-        public void getAllHostingUnits()
+        public IEnumerable<Order> getAllOrders()//TODO Func<Guest,bool> predicat=null
         {
-            throw new NotImplementedException();
+            return from allArd in dataSource.orders
+                   select allArd.Clone();
         }
 
-        public void getAllGuests()
+        public IEnumerable<Guest> getAllGuest()//TODO Func<Guest,bool> predicat=null
         {
-            throw new NotImplementedException();
+            return from gst in dataSource.guests 
+                   select gst.Clone();
         }
-
-        public void getAllOrders()
+        
+        public IEnumerable<HostingUnit> getAllHostingUnits()//TODO Func<Guest,bool> predicat=null
         {
-            throw new NotImplementedException();
+            return from hstUnit in dataSource.hostingUnits
+                   select hstUnit.Clone();
         }
-
-        public void getAllBankBranches()
+        public IEnumerable<BankAccunt> getAllBankBranches() 
         {
-            throw new NotImplementedException();
-        }
-
-        public 
+            return from Baccunt in dataSource.bankAccunt
+                   select Baccunt.Clone();
+        }  
     }
 }
+
