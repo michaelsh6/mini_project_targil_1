588e7944f726aa6f48c7d31d4aeb465869134cac
shimizra <57220385+shimizra@users.noreply.github.com>
2019-12-30 12:15:24 +0200

    class Cloning הוספת

---
+++ ProjectDotNet/DAL/Cloning.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace DAL
+{
+    public static class Cloning
+    {
+        public static T Clone<T>(this T source)
+        {
+            var isNotSerializable = !typeof(T).IsSerializable;
+            if (isNotSerializable)
+                throw new ArgumentException("The type must be serializable.", "source");
+            var sourceIsNull = ReferenceEquals(source, null);
+            if (sourceIsNull)
+                return default(T);
+            var formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
