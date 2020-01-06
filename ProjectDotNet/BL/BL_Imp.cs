07f40a756a4a132b5cec08d624bbc9479defbe59
shimizra <57220385+shimizra@users.noreply.github.com>
2020-01-06 15:12:58 +0200

    add IsDigitsOnly

--- ProjectDotNet/BL/BL_Imp.cs
+++ ProjectDotNet/BL/BL_Imp.cs
@@ -249,5 +249,16 @@ namespace BL
              var addr = new System.Net.Mail.MailAddress(email);
               return addr.Address == email;
         }
+        
+        public bool IsDigitsOnly(string str)
+        {
+             foreach (char c in str)
+              {
+                 if (c < '0' || c > '9')
+                    return false;
+               }
+            return true;
+         }
+	
     }
 }
