b6d6857b8965d3c1cc3c6a187d8181c37dd91eb8
shimizra <57220385+shimizra@users.noreply.github.com>
2020-01-28 23:53:26 +0200

    add Exceptions

--- ProjectDotNet/BE/ExseptionClasses.cs
+++ ProjectDotNet/BE/ExseptionClasses.cs
@@ -7,9 +7,24 @@ using System.Threading.Tasks;
 namespace BE
 {
    
-
+    //Exception class:
+   
     public class DateMismatchException : Exception { public DateMismatchException(string message) : base(message) { } }
 
-    public class test : Exception{ public test(string message) : base(message){ }}
-
+    public class MailValiditionException : Exception{ public MailValiditionException(string message) : base(message){ }}
+   
+   public class IncorrectPhoneException : Exception{ public IncorrectPhoneException(string message) : base(message){ }}
+   
+   public class UnitIsAvailablException : Exception{ public UnitIsAvailablException(string message) : base(message){ }}
+   
+   public class StartStatusException : Exception{ public StartStatusException(string message) : base(message){ }}
+   
+   public class GuestStatusException : Exception{ public GuestStatusException(string message) : base(message){ }}
+   
+   public class NoBankCreditException : Exception{ public NoBankCreditException(string message) : base(message){ }}
+ 
+   public class NoOpenOrderException : Exception{ public NoOpenOrderException(string message) : base(message){ }}
+   
+   //public class NotImplementedException : Exception{ public NotImplementedException(){ }}
+  
 }
